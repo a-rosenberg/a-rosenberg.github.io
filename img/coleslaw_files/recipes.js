1910d539e38cc9f9c5b8b870a77aa0a07b23a450
a-rosenberg <aaron.matthew.rosenberg@gmail.com>
2017-03-19 20:28:18 -0600

    added red cabbage slaw

---
+++ img/coleslaw_files/recipes.js
@@ -0,0 +1,27 @@
+function getRequestedMultiplierCount(){
+  var multiplier = document.forms.multiplierInput.multiplier.value.trim();
+  return Number(multiplier);
+}
+
+function isInt(n) {
+   return n % 1 === 0;
+}
+
+function scaleRecipe(){
+	var multiplier;
+	multiplier = getRequestedMultiplierCount();
+
+	var ingredients;
+	ingredients = document.getElementsByClassName("quantity");
+	for (i in ingredients) {
+		var newQuantity = Number(eval(ingredients[i].innerHTML)) * multiplier
+		if (!isInt(newQuantity)) {
+			newQuantity = newQuantity.toFixed(2)
+		};
+		ingredients[i].innerHTML =  newQuantity;
+	};
+}
+
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+	document.getElementsById('hide-mobile').style.visibility='hidden'
+}
